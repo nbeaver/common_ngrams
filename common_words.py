bf266e5b564c5fda874874493b1f382c0ea5b911
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2017-05-27 15:51:27 -0500

    Split this off into its own function.

--- common_words.py
+++ common_words.py
@@ -7,6 +7,18 @@ def wordset(fp):
     wordlist_lower = [word.strip('.,"();"').lower() for word in wordlist]
     return set(wordlist_lower)
 
+def words_in_common(included_fp, excluded_fp):
+    include_words = []
+    for included_fp in included_fp:
+        include_words.append(wordset(included_fp))
+    exclude_words = []
+    for excluded_fp in excluded_fp:
+        exclude_words.append(wordset(excluded_fp))
+    common_words = set(set.intersection(*include_words))
+    for exclude_wordset in exclude_words:
+        common_words = common_words - exclude_wordset
+    return common_words
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find words in common for some text files.')
     parser.add_argument(
@@ -23,20 +35,8 @@ if __name__ == '__main__':
         help='Exclude words from these files.',
     )
     args = parser.parse_args()
-    include_words = []
-    for included_file in args.include:
-        include_words.append(wordset(included_file))
 
-    exclude_words = []
-    for excluded_file in args.exclude:
-        exclude_words.append(wordset(excluded_file))
+    display_words = words_in_common(args.include, args.exclude)
 
-    common_words = set(set.intersection(*include_words))
-
-    for exclude_wordset in exclude_words:
-        common_words = common_words - exclude_wordset
-
-    for word in sorted(common_words):
+    for word in sorted(display_words):
         sys.stdout.write(word + "\n")
-
-    sys.stderr.write(str(len(common_words))+"\n")
